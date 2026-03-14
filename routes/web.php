a04cc972beed7cc92c804af171f3c0a9736d0736
chomani12 <chomani12@users.noreply.github.com>
2026-03-14 04:05:27 +0300

    Add temporary password reset route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,21 @@ use App\Http\Controllers\Admin\SettingController;
 
 /*
 |--------------------------------------------------------------------------
+| Temporary Password Reset Route (REMOVE AFTER USE!)
+|--------------------------------------------------------------------------
+*/
+Route::get('/reset-admin-password', function () {
+    $user = \App\Models\User::where('email', 'admin@gardibazzar.com')->first();
+    if ($user) {
+        $user->password = \Illuminate\Support\Facades\Hash::make('password123');
+        $user->save();
+        return 'Admin password has been reset! Email: admin@gardibazzar.com | Password: password123';
+    }
+    return 'Admin user not found!';
+});
+
+/*
+|--------------------------------------------------------------------------
 | Frontend Routes
 |--------------------------------------------------------------------------
 */
